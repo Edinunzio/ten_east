749505010ee6e03da692ffa8ceb55f6308c0d262
Edinunzio <e.dinunzio@gmail.com>
2024-08-10 18:31:21 -0400

    changing logout to POST

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'portal.User'
 LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
